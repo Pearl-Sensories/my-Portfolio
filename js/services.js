8fa262fa83ac2eb8022238b53c9d455cb9acdadf
Pearl Sarpong <pearlsensory01@gmail.com>
2025-03-03 13:11:41 +0000

    initial commit

---
+++ js/services.js
@@ -0,0 +1,6 @@
+const button = document.getElementById('me');
+button.addEventListener('click', () => {
+    alert('0501458413/ 0206145007')
+
+    fetchData("https://jsonplaceholder.typicode.com/users");
+});
